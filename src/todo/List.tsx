337ffb85067c0fe6fe81bf647c3d4d9087b464e8
Tom <153046sk@gmail.com>
2021-09-19 00:58:04 +0900

    routing settings

--- src/todo/List.tsx
+++ src/todo/List.tsx
@@ -1,3 +1,12 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 import { TextField } from "@material-ui/core";
+
+class List extends React.Component{
+  render(){
+    return{
+
+    }
+  }
+}
+export default List;
--- src/todo/common/Router.tsx
+++ src/todo/common/Router.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "../../App";
+import List from "../List";
 
-class Router extends React.Component{
-  render(){
-    return {
-
-    }
-  }
+const Router = () => {
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App}/>
+      <Route path="/board" component={List}/>
+      <Route/>
+    </Switch>
+  </BrowserRouter>
 }
-export default Router
